66fb659a2e72c7779232da1a5da76d1f2ee9fe16
bryancurtis3 <curtispro97@gmail.com>
2016-06-14 11:30:43 -0400

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-file = open("names.txt", 'r')
+file = open("now.txt", 'r')
 f = file.read()
 
 domains = f.split("\n")
@@ -10,10 +10,6 @@ file = open("key.txt", 'r')
 lines = file.read()
 lines = lines.lower()
 key = lines.split("\n")
-#print(lines)
-
-blue = []
-
 
 agent = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
 
@@ -21,35 +17,35 @@ for d in range(len(domains)):
     hits = 0
     try:
         response = requests.get('http://' + domains[d], headers = agent)
-
+        
         blue = response.text
         blue = BeautifulSoup(blue, 'html.parser')
         blue = blue.get_text()
         blue = blue.lower()
-        purple = blue.split(" ")
         red = blue
         
-        #print(blue)
         
-        #for x in range(len(purple)):
-            
         for n in range(len(key)):
-            
-            hits += red.count(key[n])
+            hits = 0
+            loc = 0
+            hits = red.count(key[n])
+            loc = red.find(key[n])
 
-        if hits > 0:
-            print(domains[d], "\t", hits)
+
+            if hits > 0:
+                print(domains[d], "\t", key[n] + ":", hits)
+                
+            if loc != -1:
+                for j in range(hits):
+                    num = j+1
+                    surround = red[loc-100:loc+100]
+                    surround = surround.replace("\n", '')
+                    surround = surround.replace("\t", '')
+                    print("Hit %d:" % num)
+                    print(surround, "\n----------------------------------------")
+                    loc = red.find(key[n], loc + len(key[n]))
+                
+                
     except:
         print(domains[d], "unretrievable")
         #print('-')
-
-
-# Model for single keyword
-"""
-wills = 0
-for x in range(len(red)):
-    if "will" in red[x]:
-        wills += 1
-        #print("will")
-print(wills)
-"""
